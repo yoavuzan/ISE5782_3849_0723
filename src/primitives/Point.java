6c7d712f799b2a3a57250460577f819ef39b8967
yaniv051293@gmail.com <yaniv051293@gmail.com>
2022-02-28 12:49:17 +0200

    point and vector

--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -1,4 +1,64 @@
 package primitives;
-
+/**
+ * This class will reperesent point
+ *
+ * @author Yoav uzan and yaniv bartov
+ */
 public class Point {
+
+    final Double3 xyz;
+
+    /**
+     * primary constructor for Point
+     * @param xyz Double3 value for x,z,z axis
+     */
+    public Point(Double3 xyz){
+        this.xyz=xyz;
+    }
+
+    /**
+     * secondary constructor for Point
+     *
+     * @param x coordinate value for X axis
+     * @param y coordinate value for Y axis
+     * @param z coordinate value for Z axis
+     */
+    public Point(double x,double y,double z){
+        xyz=new Double3(x,y,z)
+    }
+
+    public Double3 getXyz() {
+        return xyz;
+    }
+
+    /**
+     * add vetctor to point
+     * @param vector
+     * @return the new point
+     */
+    public Point add(Vector vector){
+        return  new Point(xyz.add(vector.xyz));
+    }
+
+    /**
+     * subtract point from vector
+     * @param point
+     * @return the new vector
+     */
+    public Vector subtract(Point point){
+        return new Vector(xyz.subtract(point.xyz));
+    }
+
+
+    @Override
+    public boolean equals(Object obj){
+        if(this==obj) return true;
+        if(obj==null) return false;
+        if(!(obj instanceof Point))return false;
+        Point other=(Point) obj;
+        return xyz.equals(other.xyz);
+    }
+
+    @Override
+    public  Sring toString(){ return xyz.toString()}
 }
--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -1,4 +1,113 @@
 package primitives;
 
-public class Vector {
+public class Vector extends Point {
+
+    /**
+     * primary  constructor for Vector class
+     *
+     * @param xyz head of vector starting from origin Point(0.0.0)
+     */
+    public Vector(Double3 xyz) {
+        super(xyz);
+        if (xyz.equals(Double3.ZERO)) {
+            throw new IllegalArgumentException("Vector(0,0,0) is not allowed");
+        }
+    }
+
+    public Vector(double x, double y, double z) {
+        this(new Double3(x, y, z));
+    }
+
+    /**
+     * @return length Squared
+     */
+    public double lengthSquared() {
+        return xyz.d1 * xyz.d1
+                + xyz.d2 * xyz.d2
+                + xyz.d3 * xyz.d3;
+    }
+
+    /**
+     *
+     * @param other
+     * @return
+     */
+    public Vector add(Vector other) {
+        return  new Vector(xyz.add(other.xyz));
+}
+
+    /**
+     * Scale (multiply) floating point triad by a number into a new triad where each
+     * number is multiplied by the number
+     *
+     * @param rhs right handle side operand for scaling
+     * @return result of scale
+     */
+    public Vector scale(double rhs) {
+        return xyz.scale(rhs);
+    }
+
+    /**
+     * @return length of vector
+     */
+    public double length() {
+        return Math.sqrt(lengthSquared());
+    }
+
+    /**
+     * dot product between two vectors (scalar product)
+     *
+     * @param other the right vector of U.V
+     * @return scalar value of the dot product
+     * @link https://www.mathsisfun.com/algebra/vectors-dot-product.html
+     */
+    public double dotProduct(Vector other) {
+        return xyz.d1 * other.xyz.d1
+                + xyz.d2 * other.xyz.d2
+                + xyz.d3 * other.xyz.d3;
+    }
+
+    /**
+     * cross product between two vectors (vectorial product)
+     * @param other other the right vector of U.V
+     * @return the vector resulting from the cross product (Right-hand rule)
+     * @link https://www.mathsisfun.com/algebra/vectors-cross-product.html
+     */
+    public Vector crossProduct(Vector other) {
+        double ax = xyz.d1;
+        double ay = xyz.d2;
+        double az = xyz.d3;
+
+        double bx = other.xyz.d1;
+        double by = other.xyz.d2;
+        double bz = other.xyz.d3;
+
+        double cx = ay * bz - az * by;
+        double cy = az * bx - ax * bz;
+        double cz = ax * by - ay * bx;
+
+        return new Vector(cx, cy, cz);
+    }
+
+    /**
+     *
+     * @return
+     */
+    public Vector normalize() {
+        double len = length();
+        return new Vector(xyz.reduce(len));
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (!(obj instanceof Point)) return false;
+        Point other = (Point)obj;
+        return super.equals(other);
+    }
+
+    @Override
+    public String toString() { return "->" + super.toString(); }
+
 }
