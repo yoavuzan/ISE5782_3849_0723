70d436f1d86c8ff133ea8cb06f6cf8fafd37cc8f
yoav uzan <yoav547@gmail.com>
2022-03-01 22:53:00 +0200

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+
+
 import primitives.*;
 import static java.lang.System.out;
 import static primitives.Util.*;
--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -1,5 +1,5 @@
 package primitives;
-<<<<<<< HEAD
+
 /**
  * This class will reperesent point
  *
@@ -11,10 +11,11 @@ public class Point {
 
     /**
      * primary constructor for Point
+     *
      * @param xyz Double3 value for x,z,z axis
      */
-    public Point(Double3 xyz){
-        this.xyz=xyz;
+    public Point(Double3 xyz) {
+        this.xyz = xyz;
     }
 
     /**
@@ -24,8 +25,8 @@ public class Point {
      * @param y coordinate value for Y axis
      * @param z coordinate value for Z axis
      */
-    public Point(double x,double y,double z){
-        xyz=new Double3(x,y,z)
+    public Point(double x, double y, double z) {
+        xyz = new Double3(x, y, z);
     }
 
     public Double3 getXyz() {
@@ -34,28 +35,29 @@ public class Point {
 
     /**
      * add vetctor to point
+     *
      * @param vector
      * @return the new point
      */
-    public Point add(Vector vector){
-        return  new Point(xyz.add(vector.xyz));
+    public Point add(Vector vector) {
+        return new Point(xyz.add(vector.xyz));
     }
 
     /**
      * subtract point from vector
+     *
      * @param point
      * @return the new vector
      */
-    public Vector subtract(Point point){
+    public Vector subtract(Point point) {
         return new Vector(xyz.subtract(point.xyz));
     }
 
     /**
-     *
      * @param other
-     * @return  d = ((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1) + (z2 - z1)*(z2 - z1))
+     * @return d = ((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1) + (z2 - z1)*(z2 - z1))
      */
-    public double distanceSquared(Point other){
+    public double distanceSquared(Point other) {
         double x1 = xyz.d1;
         double y1 = xyz.d2;
         double z1 = xyz.d3;
@@ -64,34 +66,30 @@ public class Point {
         double y2 = other.xyz.d2;
         double z2 = other.xyz.d3;
 
-        return ((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1) + (z2 - z1)*(z2 - z1));
+        return ((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
     }
 
     /**
-     *
      * @param other
      * @return d = Sqrt (lengthSquare)
      * @link https://www.engineeringtoolbox.com/distance-relationship-between-two-points-d_1854.html
      */
-    public  double distance (Point other){
+    public double distance(Point other) {
         return Math.sqrt(distanceSquared(other));
     }
 
 
-
     @Override
-    public boolean equals(Object obj){
-        if(this==obj) return true;
-        if(obj==null) return false;
-        if(!(obj instanceof Point))return false;
-        Point other=(Point) obj;
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (!(obj instanceof Point)) return false;
+        Point other = (Point) obj;
         return xyz.equals(other.xyz);
     }
 
     @Override
-    public  Sring toString(){ return xyz.toString()}
-=======
-
-public class Point {
->>>>>>> initial commit
+    public String toString() {
+        return xyz.toString();
+    }
 }
