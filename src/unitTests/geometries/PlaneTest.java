5fe0b0cc453098a6cd40a76a2d07dc7bcb129bf4
yoav uzan <yoav547@gmail.com>
2022-03-23 11:56:55 +0200

    final targil threes

--- src/unitTests/geometries/PlaneTest.java
+++ src/unitTests/geometries/PlaneTest.java
@@ -4,39 +4,45 @@ import geometries.Plane;
 import geometries.Polygon;
 import org.junit.jupiter.api.Test;
 import primitives.Point;
+import primitives.Ray;
 import primitives.Vector;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 /**
  * Unit tests for Geometries.Plane class
+ *
  * @author Yoav Uzan and Yaniv Bartov
  */
 class PlaneTest {
 
-    public void TestConstuctorPlane()
-    {
+    public void TestConstuctorPlane() {
         // TC01: Correct concave quadrangular with vertices in correct order
-        try
-        { new Plane(new Point(0, 0, 1), new Point(1, 0, 0),new Point(0,1,0));}
-         catch(IllegalArgumentException e)
-        {fail("Failed constructing a correct plane");}
+        try {
+            new Plane(new Point(0, 0, 1), new Point(1, 0, 0), new Point(0, 1, 0));
+        } catch (IllegalArgumentException e) {
+            fail("Failed constructing a correct plane");
+        }
     }
-void TestPlane(){
+
+    void TestPlane() {
         //============ Boundary Values Test=============
-    // Tow point converge
+        // Tow point converge
 
-    assertThrows(IllegalArgumentException.class,()->new Plane(
-                    new Point(0, 0, 1),
-                    new Point(0, 0, 1),
-                    new Point(0, 1, 0)),
-            "constructor for 3 points does not throw an exception in case of 2 same points");
+        assertThrows(IllegalArgumentException.class, () -> new Plane(
+                        new Point(0, 0, 1),
+                        new Point(0, 0, 1),
+                        new Point(0, 1, 0)),
+                "constructor for 3 points does not throw an exception in case of 2 same points");
 
-    //test all points on same line
-    assertThrows(IllegalArgumentException.class,()->new Plane(
-                    new Point(1, 1, 1),
-                    new Point(2, 2, 2),
-                    new Point(3, 3, 3)),
-            "constructor for 3 points does not throw an exception in case of all points on same line");
+        //test all points on same line
+        assertThrows(IllegalArgumentException.class, () -> new Plane(
+                        new Point(1, 1, 1),
+                        new Point(2, 2, 2),
+                        new Point(3, 3, 3)),
+                "constructor for 3 points does not throw an exception in case of all points on same line");
     }
 
 
@@ -45,13 +51,26 @@ void TestPlane(){
      */
     @Test
     void testGetNormal() {
-    Plane plane=new Plane(
-            new Point(0,0,1),
-            new Point(1,0,0),
-            new Point(0,1,0));
+        Plane plane = new Plane(
+                new Point(0, 0, 1),
+                new Point(1, 0, 0),
+                new Point(0, 1, 0));
 
-    double sqrt=Math.sqrt(1d/3);//point for check
-    Vector normalCheck=plane.getNormal(new Point(0,0,1));
-        assertEquals(new Vector(sqrt, sqrt, sqrt), normalCheck,"Bad normal to plane");
+        double sqrt = Math.sqrt(1d / 3);//point for check
+        Vector normalCheck = plane.getNormal(new Point(0, 0, 1));
+        assertEquals(new Vector(sqrt, sqrt, sqrt), normalCheck, "Bad normal to plane");
     }
+
+
+    /**
+     * Test  Find Intersection
+     */
+    @Test
+    void testFindIntersection() {
+        Plane plane = new Plane(new Point(1, 0, 0), new Point(0, 1, 0), new Point(0, 0, 1));
+
+
+    }
+
+
 }
