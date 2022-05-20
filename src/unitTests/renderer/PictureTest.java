d403b61171142a752f45f1f449cadc2180708314
yaniv051293@gmail.com <yaniv051293@gmail.com>
2022-05-20 10:39:58 +0300

    our picture

--- src/unitTests/renderer/PictureTest.java
+++ src/unitTests/renderer/PictureTest.java
@@ -21,33 +21,26 @@ public class PictureTest {
         scene.setAmbientLight(new AmbientLight(new Color(java.awt.Color.WHITE), new Double3(0.15)));
 
         scene.geometries.add( //
-                new Sphere(new Point(0,50,-800),120) //the turquise sphere
-                        .setEmission(new Color(0,100,100)) //
-                        .setMaterial(new Material().setKd(0.6).setKs(0.9).setShininess(1000).setKt(0).setKr(1)),
-                new Sphere(new Point(-210,110,-900),300) //the large purple sphere
-                        .setEmission(new Color(30,0,50)) //
-                        .setMaterial(new Material().setKd(0).setKs(0.2).setShininess(1000).setKt(0.6).setKr(0.2)),
-                new Sphere(new Point(300,50,-200),80) //the blue sphere (not seen boldly)
-                        .setEmission(new Color(0,0,100)) //
-                        .setMaterial(new Material().setKd(0.6).setKs(0.9).setShininess(1000).setKt(0).setKr(0.2)),
-                new Sphere(new Point(10, 30, -100),10) //the red sphere
-                        .setEmission(new Color(java.awt.Color.RED)) //
-                        .setMaterial(new Material().setKd(0.25).setKs(0.25).setShininess(20).setKt(0).setKr(0.8)),
-                new Sphere(new Point(-80, 80, -400),20) //the magenta sphere
-                        .setEmission(new Color(java.awt.Color.MAGENTA)) //
-                        .setMaterial(new Material().setKd(0.25).setKs(0.25).setShininess(20).setKt(0.7).setKr(0)),
-                new Sphere(new Point(-100, 60, -300),30) //the yellow sphere
-                        .setEmission(new Color(java.awt.Color.YELLOW)) //
-                        .setMaterial(new Material().setKd(0.25).setKs(0.25).setShininess(20).setKt(0.7).setKr(0)),
-                new Triangle(new Point(1500, -1500, -1500), new Point(-1500, 1500, -1500),//the triangle
-                        new Point(-1500, -1500, -2000)).setEmission(new Color(java.awt.Color.BLACK))
-                        .setMaterial(new Material().setKd(0.25).setKs(0.9).setShininess(20).setKt(0).setKr(0.8)));
+         //       new Plane(new Point(-10, 10, -600), new Point(0, 10, -600), new Point(-5, -3, -600))
+           //             .setEmission(new Color(0,0,100)) //
+             //           .setMaterial(new Material().setKd(0.1).setKs(0.1).setShininess(100).setKt(1).setKr(0)),
+                new Sphere(new Point(100,-100,-600),50)
+                        .setEmission(new Color(150,100,0)) //
+                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(1000).setKt(0).setKr(1)),
+        new Polygon(new Point(-10, 10, -60), new Point(-10, 20, -60), new Point(0, 20, -60), new Point(0, 10, -60))
+                .setEmission(new Color(100,100,100)) //
+                .setMaterial(new Material().setKd(0.1).setKs(0.1).setShininess(1000).setKt(0.5).setKr(0.1)),
+        new Triangle(new Point(-10, 10, -60), new Point(0, 10, -60), new Point(-5, -3, -60))
+                .setEmission(new Color(200,0,0)) //
+                .setMaterial(new Material().setKd(0.1).setKs(0.1).setShininess(1000).setKt(0).setKr(1)));
 
 
-        scene.lights.add(new SpotLight(new Color(200,200,200), new Point(1000,-550,1000), new Vector(0,-1,0)) //
+        scene.lights.add(new SpotLight(new Color(200,200,200), new Point(10,50,-100), new Vector(0,-1,0)) //
                 .setKl(4E-5).setKq(2E-7));
-        scene.lights.add(new PointLight(new Color(300, 500, 500), new Point(-50, -50, 50))//
-                .setKl(0.00001).setKq(0.000001));
+        scene.lights.add(new SpotLight(new Color(700, 400, 400), new Point(40, 40, 115), new Vector(-1, -1, -4)) //
+                .setKl(4E-4).setKq(2E-5));
+        scene.lights.add(new SpotLight(new Color(1000, 600, 0), new Point(-100, -100, 500), new Vector(-1, -1, -2)) //
+                .setKl(0.0004).setKq(0.0000006));
 
         ImageWriter imageWriter = new ImageWriter("ourPicture", 600, 600);
         camera.setImageWriter(imageWriter) //
